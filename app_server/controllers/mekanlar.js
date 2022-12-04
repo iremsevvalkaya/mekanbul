ba15adfc4b84e9bcbf65cd81c5aa5290dd223029
iremsevvalkaya <iremsevvalceng@gmail.com>
2022-12-04 23:41:05 +0300

    7.hafta

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,84 +1,107 @@
 var express = require("express");
 var router = express.Router();
+const axios = require("axios");
 
-// slash gördüğün zaman function çalışır
-const anaSayfa = function (req, res, next) {
-  res.render("anasayfa", {
-    "baslik": "Anasayfa",
-    "sayfaBaslik": {
-      "siteAd": "Mekanbul",
-      "slogan": "Civardaki Mekanları Keşfet!",
+var apiSecenekleri = {
+  sunucu:"https://mekanbul.iremsevvalkaya.repl.co",
+  apiYolu:"/api/mekanlar/",
+};
+var mesafeyiFormatla = function(mesafe){
+  var yeniMesafe, birim;
+  if(mesafe>1){
+    yeniMesafe = parseFloat(mesafe).toFixed(1);
+    birim = " km";
+  }
+  else{
+    yeniMesafe = parseInt(mesafe*1000,10);
+    birim = " m";
+  }
+  return yeniMesafe + birim;
+}
+
+var anaSayfaOlustur = function(res,mekanListesi){
+  var mesaj;
+  if(!(mekanListesi instanceof Array)){
+    mesaj = "API HATASI: Birşeyler ters gitti.";
+    mekanListesi=[];
+  }
+  else{
+    if(!mekanListesi.length){
+      mesaj="Civarda herhangi bir mekan yok.";
+    }
+  }
+  res.render("anasayfa",{
+    baslik:"Anasayfa",
+    sayfaBaslik:{
+    siteAd:"Mekanbul",
+    slogan:"Mekanları Keşfet",
+  },
+  mekanlar:mekanListesi,
+  mesaj:mesaj,
+  });
+}
+const anaSayfa = function (req, res) {
+  axios.get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu,{
+    params:{
+      enlem:req.query.enlem,
+      boylam:req.query.boylam,
     },
-    "mekanlar":[
-      {
-        "ad":"Barida Kafe",
-        "adres":"Sdü Batı Kampüsü",
-        "puan":"4",
-        "mesafe":"2km",
-        "imkanlar":["Kahve","Çay","Pasta"]
-      },
-      {
-        "ad":"Gloria",
-        "adres":"Sdü Doğu Kampüsü",
-        "puan":"2",
-        "mesafe":"5km",
-        "imkanlar":["Kahve","Çay","Pasta"]
-      }
-
-
-
-    ]
+  }).then(function(response){
+    var i,mekanlar;
+    mekanlar = response.data;
+    for (i=0;i<mekanlar.length;i++){
+      mekanlar[i].mesafe=mesafeyiFormatla(mekanlar[i].mesafe);
+    }
+    anaSayfaOlustur(res,mekanlar);
+  }).catch(function(hata){
+    anaSayfaOlustur(res,hata);
+  });
+};
+var detaySayfasiOlustur = function(res,mekanDetaylari){
+  mekanDetaylari.koordinat={
+    "enlem": mekanDetaylari.koordinat[0],
+    "boylam": mekanDetaylari.koordinat[1],
+  }
+  res.render('mekanbilgisi',
+  {
+    mekanBaslik: mekanDetaylari.ad,
+    mekanDetay: mekanDetaylari
+  });
+};
+var hataGoster = function(res,hata){
+  var mesaj;
+  if(hata.response.status=404){
+    mesaj= "404, Sayfa bulunamadı!";
+  }
+  else{
+    mesaj = hata.response.status + " hatası";
+  }
+  res.status(hata.response.status)
+  res.render('error', {
+    "mesaj":mesaj
   });
 };
 
-const mekanBilgisi = function (req, res, next) {
-  res.render("mekanbilgisi",
-  { "baslik": "Mekan Bilgisi",
-   "mekanBaslik":"Barida Kafe",
-   "mekanDetay":{
-      "ad":"Barida Kafe",
-      "adres":"Sdü Batı Kampüsü",
-      "puan":"4",
-      "saatler":[
-        {
-          "gunler":"  Pazartesi-Cuma",
-          "acilis":"9:00",
-          "kapanis":"23:00",
-          "kapali": false
-        },
-        {
-          "gunler":"  Cumartesi-Pazar",
-          "acilis":"10:00",
-          "kapanis":"22:00",
-          "kapali": false
-        }
-      ],
-      "imkanlar":["Kahve","Çay","Pasta"],
-      "koordinatlar":{
-        "enlem":"37.7",
-        "boylam":"30.5"
-      },
-      "yorumlar":[
-        
-        {
-          "yorumYapan":"İrem",
-          "puan":"2",
-          "tarih":"20 Ekim 2022",
-          "yorumMetni":"düşman başına"
-        }
-      ]
-
-   }
-
-
-
-
-
-});
+const mekanBilgisi = function (req, res) {
+  axios
+  .get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu + req.params.mekanid)
+  .then(function(response){
+    detaySayfasiOlustur(res,response.data);
+  })
+  .catch(function(hata){
+    hataGoster(res,hata);
+  })
 };
+
 
 const yorumEkle = function (req, res, next) {
-  res.render("yorumekle", { title: "Yorum Sayfası" });
+  res.render("yorumekle", { title: "Yorum Ekle" });
 };
 
-module.exports = { anaSayfa, mekanBilgisi, yorumEkle };
+module.exports = {
+  anaSayfa,
+  mekanBilgisi,
+  yorumEkle,
+  anaSayfaOlustur,
+  mesafeyiFormatla
+};
